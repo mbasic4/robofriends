7ae2a5827527c71fce39646c007898d3c5d8290d
mbasic4 <gnomnagljivi@gmail.com>
2021-10-02 19:23:13 +0200

    add favicon plus optimization

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -4,6 +4,7 @@ export const SearchBox = ({ searchField, onSearchChange }) => {
   return (
     <div className='pa2'>
       <input
+        aria-label='search-robots'
         className='pa3 ba b--green bg-washed-yellow'
         type='search'
         placeholder='Search robots...'
